40eadd5ba25af030bb48e8d986097c3d17f0b2e1
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-25 15:18:51 +0900

    sample5-2

--- src/main/java/oit/is/janken05/kazi/janken05/controller/Sample51Controller.java
+++ src/main/java/oit/is/janken05/kazi/janken05/controller/Sample51Controller.java
@@ -1,17 +1,18 @@
 package oit.is.janken05.kazi.janken05.controller;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.springframework.ui.ModelMap;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-///import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+import oit.is.janken05.kazi.janken05.model.FruitMapper;
+import oit.is.janken05.kazi.janken05.model.Fruit;
 
 /**
  * /sample5へのリクエストを扱うクラス authenticateの設定をしていれば， /sample5へのアクセスはすべて認証が必要になる
@@ -21,11 +22,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Sample51Controller {
 
   @Autowired
+  FruitMapper fMapper;
 
   @GetMapping("step1")
   public String sample51() {
     return "sample51.html";
   }
 
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
 
+  @GetMapping("step3")
+  @Transactional
+  public String sample53(@RequestParam Integer id, ModelMap model) {
+    // 削除対象のフルーツを取得
+    Fruit fruit3 = fMapper.selectById(id);
+    model.addAttribute("fruit3", fruit3);
+
+    // 削除
+    fMapper.deleteById(id);
+
+    // 削除後のフルーツリストを取得
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
 }
