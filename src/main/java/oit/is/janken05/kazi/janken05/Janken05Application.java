74209ee77efffb12bbb2c85166f887343ce89a57
e1b20038 <e1b20038@oit.ac.jp>
2022-10-25 14:01:51 +0900

    initialize

---
+++ src/main/java/oit/is/janken05/kazi/janken05/Janken05Application.java
@@ -0,0 +1,13 @@
+package oit.is.janken05.kazi.janken05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken05Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/janken05/kazi/janken05/Janken05ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.janken05.kazi.janken05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
